681b7012cb703992e8d5ddd9f3b6c543aded6113
Jeff Segars <jeff@webempoweredchurch.org>
2006-01-31 23:47:24 +0000

    Moving from tags to trunk.

---
+++ ext_emconf.php
@@ -0,0 +1,37 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "wec_sermons"
+# 
+# Auto generated 31-01-2006 12:13
+# 
+# Manual updates:
+# Only the data in the array - anything else is removed by next write
+########################################################################
+
+$EM_CONF[$_EXTKEY] = Array (
+	'title' => 'WEC Sermons',
+	'description' => 'Management of Sermon Resources',
+	'category' => 'plugin',
+	'author' => 'Web Empowered Church Team, Foundation For Evangelism',
+	'author_email' => 'wec_sermons@webempoweredchurch.org',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'private' => '',
+	'download_password' => '',
+	'version' => '0.0.0',	// Don't modify this! Managed automatically during upload to repository.
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"6dfc";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"a6dc";s:20:"doc/wizard_form.html";s:4:"7b85";}',
+);
+
+?>
